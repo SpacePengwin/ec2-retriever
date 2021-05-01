719605894b84bb9951473865cdc97a6dcc0fc4b2
SpacePengwin <11810380+SpacePengwin@users.noreply.github.com>
2021-05-01 19:02:35 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,181 @@
+use std::process::Command;
+use serde_json;
+use serde::{Deserialize, Serialize};
+
+fn main() {
+    let instances = get_instances();
+
+    for instance in instances.iter() {
+        let msg = format!("Instance ID: {} || Instance Public IP: {}", instance.InstanceId, instance.PublicIpAddress);
+        println!("{}", &msg); // Example of simplicity with structs
+    }
+
+
+}
+
+fn get_instances() -> Vec<Instance> {
+    let mut instances: Vec<Instance> = Vec::new();
+    let command = Command::new("aws")
+        .arg("ec2")
+        .arg("describe-instances")
+        .output()
+        .expect("Failed to get ec2 instances.");
+
+    let output = String::from_utf8_lossy(&command.stdout);
+    let ec2_reservations: EC2 = serde_json::from_str(&output)
+    .expect("Failed to create EC2 object...");
+    for reservation in ec2_reservations.Reservations.iter() {
+        for instance in reservation.Instances.iter() {
+            let owned = instance.clone();
+            instances.push(owned);
+        }
+    }
+    return instances;
+}
+
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct EC2 {
+    Reservations: Vec<Instances>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct Instances {
+    Instances: Vec<Instance>
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Instance {
+    AmiLaunchIndex: i32,
+    ImageId: String,
+    InstanceId: String,
+    InstanceType: String,
+    KeyName: String,
+    LaunchTime: String,
+    PublicDnsName: String,
+    PublicIpAddress: String,
+    Tags: Vec<Tags>,
+    Monitoring: Monitoring,
+    Placement: Placement,
+    PrivateDnsName: String,
+    PrivateIpAddress: String,
+    State: State,
+    StateTransitionReason: String,
+    SubnetId: String,
+    VpcId: String,
+    Architecture: String,
+    BlockDeviceMappings: Vec<BlockDevice>,
+    ClientToken: String,
+    EbsOptimized: bool,
+    EnaSupport: bool,
+    Hypervisor: String,
+    NetworkInterfaces: Vec<NetworkInterface>,
+    RootDeviceName: String,
+    RootDeviceType: String,
+    SecurityGroups: Vec<Group>,
+    SourceDestCheck: bool
+
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct NetworkInterface {
+    Association: Association,
+    Attachment: Attachment,
+    Description: String,
+    Groups: Vec<Group>,
+    MacAddress: String,
+    NetworkInterfaceId: String,
+    OwnerId: String,
+    PrivateDnsName: String,
+    PrivateIpAddress: String,
+    PrivateIpAddresses: Vec<PrivateIpAddress>,
+    SourceDestCheck: bool,
+    Status: String,
+    SubnetId: String,
+    VpcId: String,
+    InterfaceType: String
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct PrivateIpAddress {
+    Association: Association,
+    Primary: bool,
+    PrivateDnsName: String,
+    PrivateIpAddress: String
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Attachment {
+    AttachTime: String,
+    AttachmentId: String,
+    DeleteOnTermination: bool,
+    DeviceIndex: i32,
+    Status: String,
+    NetworkCardIndex: u32
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Group {
+    GroupName: String,
+    GroupId: String
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Association {
+    IpOwnerId: String,
+    PublicDnsName: String,
+    PublicIp: String
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Ebs {
+    AttachTime: String,
+    DeleteOnTermination: bool,
+    Status: String,
+    VolumeId: String
+}
+
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct BlockDevice {
+    DeviceName: String,
+    Ebs: Ebs
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Placement {
+    AvailabilityZone: String,
+    GroupName: String,
+    Tenancy: String
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct State {
+    Code: i32,
+    Name: String
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Tags {
+    Key: String,
+    Value: String
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Monitoring {
+    State: String
+}
